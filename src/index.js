7c5d1338e3d75899c877c92c314fbd0b8225a772
Alec Larson <alec.stanford.larson@gmail.com>
2019-07-02 22:43:52 -0400

    chore: run prettier

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
-const debug = require('debug')('loop:find-module-bin');
 const fs = require('fs');
 const path = require('path');
+const debug = require('debug')('loop:find-module-bin');
 
-var nodeModulePaths = require('global-paths')();
+const nodeModulePaths = require('global-paths')();
 
-module.exports = function findBinFile (binFileName) {
-  
-  var nmp = nodeModulePaths.pop();
-
-  if ( ! nmp) throw new Error(`could not find ${binFileName}`);
-
+module.exports = function findBinFile(binFileName) {
+  const nmp = nodeModulePaths.pop();
+  if (!nmp) {
+    throw new Error(`could not find ${binFileName}`);
+  }
   try {
-
     const p = path.join(nmp, '.bin', binFileName);
-
     debug(`looking for ${binFileName} at ${p}`);
 
     nmp = fs.statSync(p);
-
     debug(`found ${binFileName} at ${p}`);
 
     return p;
-
   } catch (e) {
-
     return findBinFile(binFileName);
-
   }
-
-}
+};
--- test/index.js
+++ test/index.js
@@ -1,15 +1,10 @@
-const find = require('../');
+const find = require('..');
 const path = require('path');
 const should = require('should');
 
-describe('find-module-bin', function () {
-
-  it('should find bin in approriate folder', function () {
-
+describe('find-module-bin', () => {
+  it('should find bin in approriate folder', () => {
     const filepath = find('babel');
-
     filepath.should.eql(path.join(__dirname, '..', 'node_modules', '.bin', 'babel'));
-
   });
-
 });
